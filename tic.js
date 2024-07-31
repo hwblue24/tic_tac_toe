f306769a61e79961f39fd3980811e06a839d22a4
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2024-07-31 12:13:00 -0400

    Set up array to store all instances of player objects

--- tic.js
+++ tic.js
@@ -1,57 +1,43 @@
 //handles game assets 
 const TicTacToe = (function () {
     const gameBoard = [[0,1,2], [0,1,2], [0,1,2]]; 
+    let playerList = []; 
+
+    const getPlayerList = () => playerList; 
     
     function createPlayer (initialName, intialMarker) {
+        
+
         let name = initialName; 
         let marker = intialMarker 
+
+    
+        playerList.push(name)
+        
+
+        const getPlayerList = () => playerList;
+       
         const getName = () => name;
         const getMarker = () => marker; 
         const setName = (newName) => name = newName;
         const setMarker = (newMarker) => marker = newMarker
+        
 
-        return{getName, getMarker,setName, setMarker}
+        return{getName, getMarker,setName, setMarker,getPlayerList}
     
     }
     
-    return {gameBoard, createPlayer}
+    return {gameBoard, createPlayer, getPlayerList}
 
 })();  
 
 //IIFE handles gameflow 
 const gameController = (function () {
-    let round = 1; 
-    function playRound () {
-        if (round % 2 == 1) { 
-            console.log('Player 1 turn');
-            console.log(TicTacToe.gameBoard)
-            
-            insertMarker ('X'); 
-            addRound();
-            
 
-        } else {
-            console.log ('Player 2 turn');
-            console.log( TicTacToe.gameBoard)
-    
-            insertMarker ('O'); 
-            addRound();
-            
-        }
-    
-    }
-
-    const addRound = () => round++; 
-    const getRound = () => round;
-
-
-    function insertMarker (marker) {
-        let subArray = prompt('provide subarray index')
-        let index = prompt('provide index number')
-        TicTacToe.gameBoard[subArray].splice(index,1, marker );
-        console.log(TicTacToe.gameBoard);
-    }
-
-    return {playRound, getRound}
 
 })()
+
+
+//handles changing nested array values 
+
+//TicTacToe.gameBoard[subArray].splice(index,1, marker );
