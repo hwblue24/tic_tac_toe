0b4521624018edce863de6e2b647c5004ea95434
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2024-07-31 17:33:53 -0400

    IIFE pattern to handle game flow logic and introduce marker check coniditon

--- tic.js
+++ tic.js
@@ -1,29 +1,27 @@
 //handles game assets 
 const TicTacToe = (function () {
     const gameBoard = [[0,1,2], [0,1,2], [0,1,2]]; 
-    let playerList = []; 
+    const playerList = []; 
 
     const getPlayerList = () => playerList; 
     
-    function createPlayer (initialName, intialMarker) {
+    function createPlayer (initialName, initialMarker) {
         
 
         let name = initialName; 
-        let marker = intialMarker 
+        let marker = initialMarker 
 
     
-        playerList.push(name)
+        playerList.push({name, marker});
         
-
-        const getPlayerList = () => playerList;
-       
         const getName = () => name;
         const getMarker = () => marker; 
         const setName = (newName) => name = newName;
         const setMarker = (newMarker) => marker = newMarker
         
 
-        return{getName, getMarker,setName, setMarker,getPlayerList}
+        return { getName, getMarker, setName, setMarker };
+        
     
     }
     
@@ -31,8 +29,56 @@ const TicTacToe = (function () {
 
 })();  
 
+//need this line otherwise player1 and player2 are empty.
+let haroon = TicTacToe.createPlayer('haroon','H');
+let dureti = TicTacToe.createPlayer('dureti','D')
+
 //IIFE handles gameflow 
 const gameController = (function () {
+
+
+    let player1 = TicTacToe.getPlayerList()[0];
+    let player2 = TicTacToe.getPlayerList()[1];
+    let round = 1; 
+
+    function gameRound () { 
+        if(round%2 ==1 ) { 
+            console.log(`${player1.name}'s turn`)
+            console.log(TicTacToe.gameBoard);
+            console.log( `Pick an array and its index to mark`)
+            let array = prompt('choose an array'); 
+            let index = prompt('choose an index');
+            let marker = player1.marker 
+            insertMarker(array,index, marker);
+            round +=1
+            
+        }else { 
+            console.log( `${player2.name}'s turn`)
+            console.log(TicTacToe.gameBoard);
+            console.log( `Pick an array and its index to mark)`)
+            let array = prompt('choose an array'); 
+            let index = prompt('choose an index');
+            let marker = player2.marker 
+            insertMarker(array,index,marker);
+            round +=1
+
+        }
+    }
+
+    function insertMarker (array,index,marker) {
+        if(TicTacToe.gameBoard[array][index] === player1.marker || TicTacToe.gameBoard[array][index] === player2.marker) { 
+            console.log( `Spot occupied`)
+            gameRound();
+        }else { 
+            TicTacToe.gameBoard[array].splice(index,1, marker );
+        }
+
+         
+    }
+
+
+
+    return { player1, player2, gameRound, insertMarker }
 
 
 })()
